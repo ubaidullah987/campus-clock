37a6e3df863f666f39edb01984deaab8c223b517
Aubaid Ullaha Ganaie <ubaidullah15261@gmail.com>
2026-04-29 05:25:06 +0530

    Change SUPABASE_KEY in config.js
    Updated SUPABASE_KEY with a new value.

--- assets/js/config..js
+++
@@ -1,8 +0,0 @@
-// Rename this file to config.js and add your real keys.
-// DO NOT commit config.js to version control.
-
-const APP_CONFIG = {
-    SUPABASE_URL: 'https://YOUR_SUPABASE_ID.supabase.co',
-    SUPABASE_KEY: 'YOUR_SUPABASE_ANON_KEY',
-    ADMIN_PIN: '3987'
-};
