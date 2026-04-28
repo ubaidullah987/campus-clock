28ae875a2a571a599f185fcdb5dfb3caeb7689a9
Aubaid Ullaha Ganaie <ubaidullah15261@gmail.com>
2026-04-29 05:25:40 +0530

    Change Supabase URL in config.js
    Updated Supabase URL in configuration.

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 // DO NOT commit config.js to version control.
 
 const APP_CONFIG = {
-    SUPABASE_URL: 'https://YOUR_SUPABASE_ID.supabase.co',
+    SUPABASE_URL: 'https://exoreyvbzloapejhxitt.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4b3JleXZiemxvYXBlamh4aXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTIxMDIsImV4cCI6MjA5Mjk2ODEwMn0.A5vmwaXA3z2OGx4qJ7UXUGzpR1E1Qk4wxZRQOytONjE',
     ADMIN_PIN: '3987'
 };
